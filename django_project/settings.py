e6e0e6b2ee5c24f315682f5395364a089bc8b432
i-maverick <in79@mail.ru>
2017-10-20 14:52:30 +0300

    update

--- django_project/settings.py
+++ django_project/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = BASE_DIR
 STATIC_URL = '/static/'
 
 LOGIN_URL = '/login/'
